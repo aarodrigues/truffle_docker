b114f0777843b59a5fedf693b4399afa413fa9e0
alano <alanoacioli@gmail.com>
2019-07-19 00:32:08 -0300

    feat: Change config-exercise.sh and add gitignote.txt
    Improvemente of config-exercise.sh script, change Dockfile to match
    script changes and add gitignore.txt to script use

--- Udacity Activity Environment/config-exercise.sh
+++ Udacity Activity Environment/config-exercise.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 cd BCND-C6-Exercises
-npm install --save-dev truffle-hdwallet-provider@1.0.6
-echo 'truffle-hdwallet has been installed!'
+cp ../gitignore.txt .gitignore
+branches=$(git branch -r)
+echo $branches
+filter_one=${branches//"origin/"/}
+filter_two=${filter_one//"HEAD -> master"/}
+echo $filter_two
+array=($filter_two)
+
+for element in "${array[@]}"
+do
+    echo "$element"
+    git checkout $element
+    cp ../gitignore.txt .gitignore
+    npm install --save-dev truffle-hdwallet-provider@1.0.6
+    git add -A
+    git commit -m "add truffle-hdwallet-provider@1.0.6"
+done
